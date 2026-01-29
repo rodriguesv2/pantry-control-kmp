1b8db2b293faee7e9e10f344bfa9dd9b4d4276bb
Rubens Rodrigues <rubensrodriguesv2@gmail.com>
2026-01-29 15:58:37 -0300

    Feat: Made Navigation works with mock stuff yet

--- composeApp/src/commonMain/kotlin/br/com/rubensrodrigues/pantry_control/navigation/AppNavHost.kt
+++ composeApp/src/commonMain/kotlin/br/com/rubensrodrigues/pantry_control/navigation/AppNavHost.kt
@@ -1,12 +1,26 @@
 package br.com.rubensrodrigues.pantry_control.navigation
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.navigation3.runtime.NavEntry
+import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.ui.NavDisplay
 
 @Composable
 fun AppNavHost() {
-    val backstack = rememberNavBackStack(config, Destination.VoiceButton)
+    val backStackList = remember { mutableStateListOf<Destination>(Destination.VoiceButton) }
 
-
+    NavDisplay(
+        backStack = backStackList,
+        onBack = { backStackList.removeLastOrNull() },
+        entryProvider = { key ->
+            when (key) {
+                Destination.VoiceButton -> NavEntry(key) { Text("Voice Button") }
+                Destination.Items -> NavEntry(key) { Text("Item") }
+            }
+        }
+    )
 }
